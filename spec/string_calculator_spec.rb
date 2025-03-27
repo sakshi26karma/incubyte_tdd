04fa6f2a6dda1971db15ed1eb004fcba38e6d21b
Sakshi Karma <sakshi26karma>
2025-03-27 18:12:44 +0530

    handle multiple numbers additions

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -16,6 +16,10 @@ RSpec.describe StringCalculator do
 
     it "returns the sum of two numbers" do
         expect(@calculator.add("1,5")).to eq(6)
-     end
+    end
+
+    it "returns the sum of multiple numbers" do
+        expect(@calculator.add("1,2,3,4")).to eq(10)
+    end
   end
 end
