5bbab10dbd31103f7e50b06c1ea761d3878b3f58
Sakshi Karma <sakshi26karma>
2025-03-28 19:13:14 +0530

    removed extra spaces

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -11,28 +11,27 @@ RSpec.describe StringCalculator do
     end
 
     it "returns the number itself when only one number is given" do
-        expect(@calculator.add("5")).to eq(5)
+      expect(@calculator.add("5")).to eq(5)
     end
 
     it "returns the sum of two numbers" do
-        expect(@calculator.add("1,5")).to eq(6)
+      expect(@calculator.add("1,5")).to eq(6)
     end
 
     it "returns the sum of multiple numbers" do
-        expect(@calculator.add("1,2,3,4")).to eq(10)
+      expect(@calculator.add("1,2,3,4")).to eq(10)
     end
 
     it "handles newlines b\w numbers" do
-        expect(@calculator.add("1\n2,3")).to eq(6)
+      expect(@calculator.add("1\n2,3")).to eq(6)
     end
 
     it "handles custom delimiters" do
-        expect(@calculator.add("//;\n1;2")).to eq(3)
+      expect(@calculator.add("//;\n1;2")).to eq(3)
     end
 
     it "raises an error for negative numbers" do
-        expect { @calculator.add("1,-2,3,-4,5,-6") }.to raise_error("Negative numbers not allowed: -2, -4, -6")
+      expect { @calculator.add("1,-2,3,-4,5,-6") }.to raise_error("Negative numbers not allowed: -2, -4, -6")
     end
-
   end
 end
